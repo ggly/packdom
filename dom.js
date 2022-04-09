fca95761d72f9d1806965331197adb3187faad35
ostone <857344759@qq.com>
2022-04-10 01:24:45 +0800

    超jquery靠近了一下

--- dom.js
+++ dom.js
@@ -1,13 +1,42 @@
-window.dom = {
-    find(name){
-        return document.querySelectorAll(name)
-    },
-    style(node,name,value){
-        node.style[name] = value   //变量做key必须放在中括号中，否则的话会变成字符串，所以不能直接node.style.name
-    },
-    each(nodelist,fn){
-        for(let i = 0;i < nodelist.length;i++){
-            fn.call(null,nodelist[i])      //这里写成fn(null,nodelist[i])也可以实现效果
-        }
+// window.dom = {
+//     find(name){
+//         return document.querySelectorAll(name)
+//     },
+//     style(node,name,value){
+//         node.style[name] = value   //变量做key必须放在中括号中，否则的话会变成字符串，所以不能直接node.style.name
+//     },
+//     each(nodelist,fn){
+//         for(let i = 0;i < nodelist.length;i++){
+//             fn.call(null,nodelist[i])      //这里写成fn(null,nodelist[i])也可以实现效果
+//         }
+//     }
+// }
+//上面的代码是自己直接封装的，并无什么编程思想可言
+//下面模仿jquery进行封装
+window.jQuery = function(selectorOrArray){
+    let elements
+    if(typeof selectorOrArray === 'string'){
+        elements = document.querySelectorAll(selectorOrArray)
+    }else if(selectorOrArray instanceof Array){
+        elements = selectorOrArray
     }
-}
+    return {
+      addClass(className){
+          for(let i = 0; i<elements.length;i++){
+              elements[i].classList.add(className)
+          }
+          return this
+      },
+      find(selector){
+        let array = []
+        for(let i = 0;i<elements.length;i++){
+            const elements2 = Array.from(elements[i].querySelectorAll(selector))
+            array = array.concat(elements2)
+        }
+        return jQuery(array)
+      }
+    }
+  }
+  
+  window.$ = window.jQuery
+  
--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-const div = dom.find('#test>.red')[0] // 获取对应的元素
-dom.style(div, 'color', 'red') // 设置 div.style.color
+// const div = dom.find('#test>.red')[0] // 获取对应的元素
+// dom.style(div, 'color', 'red') // 设置 div.style.color
 
-const divList = dom.find('.red') // 获取多个 div.red 元素
-dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+// const divList = dom.find('.red') // 获取多个 div.red 元素
+// dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
+//上面是之前的调用操作
+//下面是按jquery写了dom.js之后的调用操作
+$('#test').find('.red').addClass('newclass')
